10b4180b7841a95dec83e394a3f807d88c022ff3
Flameofwork <shvetsalexei@gmail.com>
2022-10-01 13:52:34 +0300

    final update
    final update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,83 @@
-// //импорты библиотек
-// import React, { Component, Fragment } from 'react';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { nanoid } from 'nanoid';
+//импорты библиотек
+import React, { Component, Fragment } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { nanoid } from 'nanoid';
 
+//куски секций и компонентов
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
 
-// //куски секций и компонентов
-// import ContactForm from './ContactForm/ContactForm';
-// import ContactList from './ContactList/ContactList';
-// import ContactItem from './ContactItem/ContactItem';
-// import Filter from './Filter/Filter';
+//стилистика общего контейнера и заголовков
+import { Container, Title, SecondTitle } from './App.styled';
 
+class App extends Component {
+state = {
+    contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+};
 
-// //стилистика общего контейнера
+onAddContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
 
+    if (
+    this.state.contacts.some(
+        contact => contact.name.toLowerCase() === normalizedName
+    )
+    ) {
+    Notify.failure(`${name} is already in contacts`);
+    return;
+    }
 
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
+    this.setState(prevState => ({
+    contacts: [
+        ...prevState.contacts,
+        {
+        id: nanoid(),
+        name,
+        number,
+        },
+    ],
+    }));
+};
 
+handleDelete = id => {
+    this.setState(prevState => ({
+    contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+};
 
+handleFilter = e => {
+    this.setState({ filter: e.target.value });
+};
 
+render() {
+    const { contacts, filter } = this.state;
+    let renderContacts = contacts;
 
+    renderContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter.trim())
+    );
 
+    return (
+        <Fragment>
+            <Container>
+                <Title>Phonebook</Title>                
+                    <ContactForm onSubmit={this.onAddContact} />                
+                <SecondTitle>Contacts</SecondTitle>                                   
+                    <Filter handleFilter={this.handleFilter}
+                    value={filter} />
+                    <ContactList contacts={renderContacts}
+                    handleDelete={this.handleDelete}/>                
+            </Container>
+        </Fragment>
+    );
+}
+}
 
-//   render() {    
-
-//     return (  <div className={s.container}></div>  );
-//   }
-  
-// }
-
-
-
-
-// export default App;
+export default App;
